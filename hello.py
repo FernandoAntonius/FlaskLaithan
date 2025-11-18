baaab27536153e530b30c62872d89b165650c00c
FernandoAntonius <fernandoanthonius_2428250014@mhs.mdp.ac.id>
2025-11-18 13:42:53 +0700

    post contact

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 # import flash module
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 # create an instance of the Flask class
 app = Flask(__name__, template_folder='views')
 # define a route for the root URL
@@ -12,9 +12,19 @@ def hello_world():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    if request.method == 'POST':
+        # Proses data form di sini
+        nama = request.form['nama']
+        email = request.form['email']
+        pesan = request.form['pesan']
+
+        # Tampilkan di terminal
+        print(f'Nama: {nama}, Email: {email}, Pesan: {pesan}')
+
+    title = 'Contact Page'
+    return render_template('contact.html', title=title)
 
 @app.route('/pmb')
 def pmb():
